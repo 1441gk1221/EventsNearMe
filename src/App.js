810b9a1054e1af06d11611fb962f2227765942ea
Govind <gk14411441@gmail.com>
2022-12-04 18:09:31 +0530

    frontend completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Components/Home';
+import { Routes, Route} from 'react-router-dom';
+import AddEvent from './Components/AddEvent';
+import AllEvents from './Components/AllEvents';
+import Signup from './Components/Signup';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import EventDetails from './Components/EventDetails';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ToastContainer autoClose={3000}/>
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route exact path="/addevent" element={<AddEvent/>} />
+        <Route exact path="/allevents" element={<AllEvents/>} />
+        <Route exact path="/signup" element={<Signup/>} />
+        <Route path="/eventdetails/:eventid" element={<EventDetails/>} />
+      </Routes>
     </div>
   );
 }
